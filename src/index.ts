ba826f9febec1c324429bbb48fc8c608a72f6af9
Alexander <alexander.neitzel@codineerdigital.com>
2022-12-12 16:14:13 +0100

    Fixed punctuation splitting not working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // all stop words that should not be used in the index for full text search
-const stopWords = ["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"];
+const stopWords = [" ", "a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"];
 
 export const tokenize = (input: string): string[] => {
-    return input.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase().split(" ").filter(token => !stopWords.includes(token));
+    return input.replace(/[^a-zA-Z0-9 ]/g, ' ').toLowerCase().split(" ").filter(token => !stopWords.includes(token));
 }
